38df67986dbda65c2b53bc07183d84d4ce5d01a1
Eugene Batogov <johnbat26@gmail.com>
2014-08-04 23:40:08 +0400

    add select by group

--- app/controllers/work_load_controller.rb
+++ app/controllers/work_load_controller.rb
@@ -15,8 +15,8 @@ class WorkLoadController < ApplicationController
     workloadParameters = params[:workload] || {}
 
     @first_day = sanitizeDateParameter(workloadParameters[:first_day], Date::today - 10)
-    @last_day  = sanitizeDateParameter(workloadParameters[:last_day],  Date::today + 50)
-    @today     = sanitizeDateParameter(workloadParameters[:today],     Date::today)
+    @last_day = sanitizeDateParameter(workloadParameters[:last_day], Date::today + 50)
+    @today = sanitizeDateParameter(workloadParameters[:today], Date::today)
 
     # Make sure that last_day is at most 12 months after first_day to prevent
     # long running times
@@ -27,19 +27,22 @@ class WorkLoadController < ApplicationController
 
     @issuesForWorkload = ListUser::getOpenIssuesForUsers(@usersToDisplay)
     @monthsToRender = ListUser::getMonthsInTimespan(@timeSpanToDisplay)
-    @workloadData   = ListUser::getHoursPerUserIssueAndDay(@issuesForWorkload, @timeSpanToDisplay, @today)
+    @workloadData = ListUser::getHoursPerUserIssueAndDay(@issuesForWorkload, @timeSpanToDisplay, @today)
   end
 
 
-
-private
+  private
 
   def initalizeUsers(workloadParameters)
 
     # Get list of users that are allowed to be displayed by this user
     @usersAllowedToDisplay = ListUser::getUsersAllowedToDisplay()
 
-    userIds = workloadParameters[:users].kind_of?(Array) ? workloadParameters[:users] : []
+    if  workloadParameters[:group] &&  !workloadParameters[:group].empty?
+      userIds = Group.find(workloadParameters[:group]).users.collect { |user| user.id}
+    else
+      userIds = workloadParameters[:users].kind_of?(Array) ? workloadParameters[:users] : []
+    end
     userIds.map! { |x| x.to_i }
 
     # Get list of users that should be displayed.
